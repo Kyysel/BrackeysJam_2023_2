e87cfdba5aec2dfe94c5a0c3d038e8648f4bb3e3
Kyle Waldon <kwaldonmusic@gmail.com>
2023-08-23 06:17:33 -0600

    Updated Building Icons and Building Images
    Added some code to make the icons look good in the UI. Changed the pivots of all building images to be at the bottom, so that buildings of different heights can still be placed the same.

--- Assets/Scripts/Buildings/BuildingPanel.cs
+++ Assets/Scripts/Buildings/BuildingPanel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,4 +10,28 @@ public class BuildingPanel : MonoBehaviour
     public Image icon;
     public Building building;
     public GameObject resourceCostPanel;
+
+    private void Start()
+    {
+        //calculate the size ratio of the icon
+        float a = icon.preferredWidth, b = icon.preferredHeight;
+        float greatestCommonDenominator = GCD(a, b);
+        Vector2 ratio = new Vector2(a / greatestCommonDenominator, b / greatestCommonDenominator);
+
+        //calculate the ratio'd height and width
+        RectTransform rect = icon.GetComponent<RectTransform>();
+        float newHeight = (rect.sizeDelta.x / ratio.x) * ratio.y;
+        float newWidth = (rect.sizeDelta.y / ratio.y) * ratio.x;
+
+        //change the Image RectTransform, keeping the biggest size always to 100
+        rect.sizeDelta = a >= b ? new Vector2 (rect.sizeDelta.x, newHeight) : new Vector2(newWidth, rect.sizeDelta.x);
+
+    }
+
+    //Returns the Greatest Common Denominator between two floats
+    public static float GCD(float a, float b)
+    {
+        return b == 0 ? Math.Abs(a) : GCD(b, a % b);
+    }
+
 }
