7d02f22799ce09baa1ac7f9bc3a943d8865da5fc
Kyysel <39512699+Kyysel@users.noreply.github.com>
2023-08-26 19:13:56 +0200

    Enhanced Worm Movement

--- Assets/Scripts/Worm/RotateToTarget.cs
+++ Assets/Scripts/Worm/RotateToTarget.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,21 +10,27 @@ public class RotateToTarget : MonoBehaviour
     public float rotationSpeed;
     private Vector2 direction;
     public float moveSpeed;
+    public float maxSpeed;
+    private Rigidbody2D _rb;
+    
+    public GameObject target;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(new Vector2(Mouse.current.position.x.ReadValue(), Mouse.current.position.y.ReadValue()));
-        
-        direction = cursorPos - transform.position;
-        transform.position = Vector2.MoveTowards(transform.position, cursorPos, moveSpeed * Time.deltaTime);
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate()
+    {
+        direction = target.transform.position - transform.position;
+        if (_rb.velocity.magnitude < maxSpeed && direction.magnitude > 1f)
+        {
+            _rb.AddForce(direction.normalized * moveSpeed);
+        }
         
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         
-        if (Vector2.Distance(transform.position, cursorPos) > 0.05f)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
-        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/Worm/WormController.cs
+++ Assets/Scripts/Worm/WormController.cs
@@ -12,32 +12,66 @@ public class WormController : MonoBehaviour
     public float gridSpacing = 10f;
     private Vector2[] grid;
     private float cameraRatio = 20f; //3.5f ratio to 70 units
+
+    public float smoothSpeed = 20f;
+    public Vector3 targetPos;
+    private Vector3 _targetPos;
+    public GameObject targetObject;
+    public GameObject wormHead;
+
+    private float _elapsedTime;
+    private Vector3 _startPosition;
+    public AnimationCurve curve;
     
+
     public void Start()
     {
         GetComponentInChildren<WormTail>().InitializeTail(length, segmentPrefab, this.gameObject);
         InitializeGrid();
+        NewTarget();
     }
 
     void InitializeGrid()
     {
-        // align with the buildings
-        _gridOrigin = new Vector2(mapCenter.x - (cameraRatio/2)*3.5f, _gridOrigin.y);
-        _gridSize = new Vector2((int)Math.Ceiling((mapCenter.x - _gridOrigin.x) * 2 / gridSpacing), 
+        _gridOrigin = new Vector2(mapCenter.x - (cameraRatio / 2) * 3.5f, mapCenter.y);
+        _gridSize = new Vector2((int)Math.Ceiling((mapCenter.x - _gridOrigin.x) * 2 / gridSpacing),
             (int)Math.Ceiling((mapCenter.x - _gridOrigin.x) * 2 / (2 * gridSpacing)));
-        
+
         //initialize the grid downwards
         grid = new Vector2[(int)((_gridSize.x) * (_gridSize.y))];
         for (int i = 0, y = 0; y < _gridSize.y; y++)
         {
-            for (int x=0; x < _gridSize.x; x++, i++)
+            for (int x = 0; x < _gridSize.x; x++, i++)
             {
                 grid[i] = new Vector2(_gridOrigin.x + x * gridSpacing, _gridOrigin.y - y * gridSpacing);
             }
         }
     }
+    void Update()
+    {
+        if(Vector2.Distance(wormHead.transform.position, targetPos) < 2f)
+        {
+            NewTarget();
+        }
+        _targetPos = Vector2.MoveTowards(_targetPos, targetPos, smoothSpeed * Time.deltaTime);
+        targetObject.transform.position = _targetPos;
+        
+        _elapsedTime += Time.deltaTime;
+        float percentageComplete = _elapsedTime / smoothSpeed;
+        _targetPos = Vector3.Lerp(_startPosition, targetPos, curve.Evaluate(percentageComplete));
+    }
 
-    public void TakeDamage()
+    void NewTarget()
+    {
+        // select a random point on the grid
+        int rand = UnityEngine.Random.Range(0, grid.Length);
+        targetPos = grid[rand];
+        _targetPos = wormHead.transform.position;
+        _startPosition = _targetPos;
+        _elapsedTime = 0;
+    }
+
+public void TakeDamage()
     {
         length -= 1;
         GetComponentInChildren<WormTail>().RemoveSegment();
@@ -55,5 +89,11 @@ public class WormController : MonoBehaviour
         {
             Gizmos.DrawSphere(grid[i], 0.2f);
         }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(_targetPos, 0.3f);
+        
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(targetPos, 0.3f);
     }
 }
